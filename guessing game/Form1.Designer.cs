4dd7c96550b233f1f80d2452f3f583896d9d1392
aubrcart224 <aubrcart224@ed.amdsb.ca>
2021-05-06 13:26:49 -0400

    Add project files.

---
+++ guessing game/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace guessing_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guessinggameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // guessinggameLabel
+            // 
+            this.guessinggameLabel.AutoSize = true;
+            this.guessinggameLabel.Location = new System.Drawing.Point(310, 57);
+            this.guessinggameLabel.Name = "guessinggameLabel";
+            this.guessinggameLabel.Size = new System.Drawing.Size(85, 13);
+            this.guessinggameLabel.TabIndex = 0;
+            this.guessinggameLabel.Text = "Guessing Game ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter a number between 1-100";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(284, 240);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(153, 29);
+            this.outputLabel.TabIndex = 2;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(384, 140);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(75, 23);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(278, 142);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guessinggameLabel);
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label guessinggameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.TextBox inputBox;
+    }
+}
+
---
+++ guessing game/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guessing_game
+{
+    public partial class Form1 : Form
+    {
+        //gloabl variables 
+        Random randGen = new Random();
+        double input;
+        double number;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            //variuasbles 
+            input = Convert.ToDouble(inputBox.Text);
+            number = randGen.Next(1, 100);
+
+            //caluclations 
+            if(input > number)
+            {
+                outputLabel.Text = "too high";
+            }
+            else if (input < number)
+            {
+                outputLabel.Text = "too low ";
+
+            }
+            else
+            {
+                outputLabel.Text = "You got it! ";
+            }
+            // out put / if else 
+
+        }
+    }
+}
